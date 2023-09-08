72fa3fe732031f862767f33d50e9cc3b4428f585
dzc0d3r <m4dcod3r@protonmail.com>
2023-09-08 02:24:13 +0100

    Finished assignement :tada:

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+
+
+class Circle {
+
+    constructor(radius) {
+        this.radius = radius;
+
+    }
+    /** circumference */
+    get diameter() {
+        return this.radius * 2;
+    }
+    set diameter(newDiameter) {
+
+        this.radius = newDiameter / 2;
+    }
+
+    /** circumference */
+    get circumference() {
+        return this.diameter * Math.PI;
+    }
+    set circumference(newCircumference) {
+
+        this.radius = newCircumference / (2 * Math.PI);
+    }
+    /** area */
+    get area() {
+        return this.radius ** 2 * Math.PI;
+    }
+    set area(newArea) {
+
+        this.radius = Math.sqrt(newArea / Math.PI);
+    }
+
+}
